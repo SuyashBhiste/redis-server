341da0abb1d6005157add6b8696d67ddb1630699
suyash.bhiste <suyash.bhiste@tripstack.com>
2022-01-24 18:05:28 +0530

    Stage 2 | Added read & write messages with client

--- app/server.go
+++ app/server.go
@@ -1,25 +1,38 @@
 package main
 
 import (
-	"fmt"
-	// Uncomment this block to pass the first stage
-	"net"
-	"os"
+	"bufio" // Read Input
+	"fmt"   // Send Message
+	"net"   // Connection Socket
+	"os"    // Control Program
 )
 
 func main() {
 	fmt.Println("Your code goes here!")
 
-	// Uncomment this block to pass the first stage
-
+	// Create a TCP Server
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	// Accept the client connection
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
+	}
+
+	// Chat
+	for {
+		// Read client commands
+		_, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			fmt.Print(err.Error())
+		}
+
+		// Respond to client
+		conn.Write([]byte("+PONG\r\n"))
 	}
 }
