429dd528fe727d694f85888fdc3c144664f24a4d
Suyash Bhiste <suyashbhiste0@gmail.com>
2024-12-05 23:24:21 +0530

    bug fix

--- app/server.go
+++ app/server.go
@@ -159,7 +159,7 @@ func handleCients(conn net.Conn) {
 					delete(DataStore, value[1])
 					conn.Write([]byte("$-1\r\n"));
 				} else {
-					conn.Write([]byte(encodeSimpleStrings(DataStore[value[1]])))
+					conn.Write([]byte(encodeSimpleStrings(dsVal.ttl)))
 				}
 
 				DataStoreMutex.Unlock()
@@ -167,9 +167,10 @@ func handleCients(conn net.Conn) {
 			case "SET":
 				DataStoreMutex.Lock()
 				DataStore[value[1]] = DataStoreValue{ Value: value[2] }
-				if len(value > 3){
-					expiry := strconv.Atoi(value[4])
-					DataStore[value[1]].ttl = time.Now().Add(expiry * time.Millisecond)
+				if len(value) > 3 {
+					expiry, _ := strconv.Atoi(value[4])
+					data, _ := DataStore[value[1]]
+					data.ttl = time.Now().Add(expiry * time.Millisecond)
 				}
 				DataStoreMutex.Unlock()
 				conn.Write([]byte(encodeSimpleStrings("OK")))
