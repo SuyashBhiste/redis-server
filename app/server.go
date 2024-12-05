77761fdbd5dd1c5843e2b6c86f383e6309276ad5
Suyash Bhiste <suyashbhiste0@gmail.com>
2024-12-05 18:46:04 +0530

    refactored

--- app/server.go
+++ app/server.go
@@ -103,7 +103,7 @@ func decode(msg string) []string {
 	result := []string{}
 	for i:=0; i<length; i++ {
 		index = index+1
-		fmt.Println("hi1" + index)
+		fmt.Println("hi1", index)
 
 		if msg[index] != '$' {
 			fmt.Println("Incomplete input request")
@@ -114,7 +114,7 @@ func decode(msg string) []string {
 		index = prev + strings.IndexRune(msg[index:], '\n')
 		length, _ = strconv.Atoi(msg[prev+1:index-1])
 
-		fmt.Println("hi2" + index)
+		fmt.Println("hi2", index)
 
 		result = append(result, msg[index+1:index+1+length])
 		index = index+3+length
