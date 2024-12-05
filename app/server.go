1607d27ecd3ea5c6f531ad76db123771d0ae629d
Suyash Bhiste <suyashbhiste0@gmail.com>
2024-12-05 18:47:35 +0530

    refactored

--- app/server.go
+++ app/server.go
@@ -111,7 +111,7 @@ func decode(msg string) []string {
 		}
 
 		prev := index
-		index = prev + strings.IndexRune(msg[index:], '\n')
+		index = prev + strings.IndexRune(msg[index+1:], '\n')
 		length, _ = strconv.Atoi(msg[prev+1:index-1])
 
 		fmt.Println("hi2", index)
