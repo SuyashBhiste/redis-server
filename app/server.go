5f5470634a865a4bbc98e8052a2466ced5dbc5b3
Suyash Bhiste <suyashbhiste0@gmail.com>
2024-12-05 17:04:33 +0530

    bug fix

--- app/server.go
+++ app/server.go
@@ -98,7 +98,7 @@ func decode(msg string) []string {
 	}
 
 	index := strings.IndexRune(msg, '\r')
-	length, err := strconv.Atoi(msg[1:index])
+	length, _ := strconv.Atoi(msg[1:index])
 
 	result := []string{}
 	for i:=0; i<length; i++ {
